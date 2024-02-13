5e70772328f7cc5d1ebcfc60c89f5b3ec1350507
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-02-13 14:23:43 +1100

    Implemented insert node in LinkedList

--- Data Structure/Linked List/singly.py
+++ Data Structure/Linked List/singly.py
@@ -26,6 +26,29 @@ class LinkedList:
         while temp:
             print(temp.get_value(), end=" ")
             temp = temp.get_next()
+        print()
+
+    def get_prev_node(self, position):
+        temp = self.head
+        count = 1
+
+        while count < position:
+            temp = temp.get_next()
+            count += 1
+
+        return temp
+
+    def insert_node(self, target_node, position):
+        if self.head is None:
+            self.head = target_node
+            return
+        if position == 0:
+            target_node.set_next(self.head)
+            self.head = target_node
+
+        prev_node = self.get_prev_node(position)
+        target_node.set_next(prev_node.get_next())
+        prev_node.set_next(target_node)
 
 
 linked_list = LinkedList()
@@ -38,4 +61,8 @@ fifth_node = Node(100)
 
 linked_list.head.set_next(second_node.set_next(third_node.set_next(fourth_node.set_next(fifth_node))))
 
+linked_list.show_list()
+
+new_node = Node(200)
+linked_list.insert_node(new_node, 3)
 linked_list.show_list()
