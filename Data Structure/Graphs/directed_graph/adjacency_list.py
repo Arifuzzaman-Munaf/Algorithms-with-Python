e05dabf7f0c1fdc769b60fc77fbb9ee2f67d60a3
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-02-24 22:07:15 +1100

    Implemented directed graph with adjacent list

---
+++ Data Structure/Graphs/directed_graph/adjacency_list.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        # initializing the graph as dictionary of lists
+        self.graph = defaultdict(list)
+
+    def insert_edge(self, vertex1, vertex2):
+        self.graph[vertex1].append(vertex2)
+
+    def show_graph(self):
+        for vertex in self.graph:
+            print(vertex, "=>", self.graph[vertex])
+
+
+graph = Graph()
+graph.insert_edge(1, 5)
+graph.insert_edge(1, 10)
+graph.insert_edge(3, 1)
+graph.insert_edge(5, 3)
+graph.insert_edge(5, 10)
+graph.insert_edge(10, 3)
+
+graph.show_graph()
