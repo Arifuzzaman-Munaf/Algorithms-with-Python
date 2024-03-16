2a4aca08443a51deb70aeda2355f66deaca45ac0
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2024-03-17 00:54:19 +1100

    Updated BFS algorithm

--- Algorithms/bfs.py
+++ Algorithms/bfs.py
@@ -4,22 +4,20 @@ def bfs(graph, start_node):
         return -1
 
     # create a set for tracking visited nodes
-    visited = set()
+    visited = set(start_node)
     # created a queue for going through the nodes
     queue = [start_node]
     # loop will be executed until queue is not empty
     while queue:
         # fetching the front element of the queue
         current_node = queue.pop(0)  # checking if the node already visited or not
-        # if visited then we won't print that node
-        # otherwise, we will print the node and mark it as visited
-        if current_node not in visited:
-            print(current_node, end=" ")
-            visited.add(current_node)
+        print(current_node, end=" ")
+
         # fetching the adjacent nodes of current node
         for vertex in graph[current_node]:
             if vertex not in visited:
                 queue.append(vertex)
+                visited.add(vertex)
 
 
 if __name__ == '__main__':
@@ -31,5 +29,5 @@ if __name__ == '__main__':
         '4': ['8'],
         '8': []
 
-    }
+        }
     bfs(graph, '5')
